66a110db762e67415dddc64620fee28a37a9b8f9
boxheadx <boxheadx1337@gmail.com>
2024-06-07 10:23:41 +0545

    added login route and a basic controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,9 +7,9 @@ const HttpError = require('../error/httpError');
 
 const register = async (req, res)=>{
     try{
-        const {firstname, lastname, email, password} = req.body;
+        const {username, email, password} = req.body;
 
-        if(!firstname || !lastname || !email || !password){
+        if(!username || !email || !password){
             throw new HttpError("Please provide all the details!", 400);
         }
 
@@ -21,7 +21,7 @@ const register = async (req, res)=>{
             throw new HttpError("Email already exists!", 400);
         } 
 
-        const result = await pool.query(queries.addUser, [firstname, lastname, email, hashedPassword]);
+        const result = await pool.query(queries.addUser, [username, email, hashedPassword]);
         res.status(200).send('Registration Successful!');
        
 
@@ -31,6 +31,28 @@ const register = async (req, res)=>{
 
 }
 
+const login = async (req, res)=>{
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password) throw new HttpError("Please provide email & password!", 400);
+        if(!(await pool.query(userQeries.checkEmailExists, [email])).rows.length) throw new HttpError("Email doesn't exist!", 400);
+        
+        const results = await pool.query(userQeries.checkEmailExists, [email]);
+        const user = results.rows[0];
+        const passwordMatched = await bcrypt.compare(password, user.password_hash);
+        if(!passwordMatched){
+            throw new HttpError("Invalid credentials!", 400);
+        }
+
+        res.status(200).send('Logged in successfully!');
+
+    } catch(httpError){
+        res.status(httpError.status).send(httpError.msg);
+    }
+}
+
 module.exports = {
-    register
+    register,
+    login
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { register } = require('../controllers/authController');
+const { register, login } = require('../controllers/authController');
 
 const router = Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
 
