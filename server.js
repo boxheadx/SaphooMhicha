2f5602bad5a24399df60962d66aef05ca5c1c640
boxheadx <boxheadx1337@gmail.com>
2024-07-11 22:28:02 +0545

    added cors because sop be drivin me crazy

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
 const authorRoutes = require('./routes/authorRoutes');
+const bookRoutes = require('./routes/bookRoutes');
 const fileUpload = require('express-fileupload');
+const cors = require('cors')
 
 const app = express();
 const port = 3000;
@@ -11,10 +13,12 @@ const port = 3000;
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload({ useTempFiles: true }))
+app.use(cors());
 
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/author', authorRoutes);
+app.use('/api/v1/book', bookRoutes);
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
